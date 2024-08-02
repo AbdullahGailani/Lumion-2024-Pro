453984f11ffd0b8a95bbe7609f49b37e40ac98ec
AbdullahGailani <abdullahgailani004@gmail.com>
2024-08-03 01:30:47 +0300

    creating finished task section adding animation and more

--- index.js
+++ index.js
@@ -1,48 +1,73 @@
 const tasksContainer = document.querySelector(".tasks");
+const tasksContainerFinish = document.querySelector(".tasks--finish");
 const addBtn = document.querySelector(".add");
 const inputNewTask = document.querySelector(".input");
-const dateLabel = document.querySelector(".date");
+const dateLabel = document.querySelectorAll(".date");
 const emailInput = document.querySelector(".email");
 const passwordInput = document.querySelector(".password");
 const loginBtn = document.querySelector(".login--btn");
 const loginAppContainer = document.querySelector(".login");
-const appContainer = document.querySelector(".container");
+const taskContainer = document.querySelector(".hidden__container--1");
+const finishTaskContainer = document.querySelector(".hidden__container--2");
 const greet = document.querySelector(".greet");
+const body = document.querySelector("body");
+const changeBtn = document.querySelectorAll(".change--container");
 
 // creating accounts in javascript because no back-end used !
 // using weak password to enter faster while testing
-const abdullah = {
-  email: "abdullah@maingroup.com",
-  password: 111,
-  tasks: ["study web", "go to work", "sleep"],
-  owner: "Abdullah",
-};
 
-const sabr = {
-  email: "sabr@maingroup.com",
-  password: 222,
-  tasks: ["study design", "go to work", "watch movie"],
-  owner: "Sabr",
-};
+class Account {
+  #email;
+  #password;
+  #owner;
 
-const ayad = {
-  email: "ayad@maingroup.com",
-  password: 333,
-  tasks: ["study web", "Shex chaw chawanem krd", "Xo juwan krdn"],
-  owner: "Ayad",
-};
+  constructor(email, password, tasks, owner, finishTasks) {
+    this.email = email;
+    this.password = password;
+    this.tasks = tasks;
+    this.owner = owner;
+    this.finishTasks = finishTasks;
+  }
+}
 
-const shkar = {
-  email: "shkar@maingroup.com",
-  password: 444,
-  tasks: ["xindn lo awail bun", "study to be awail", "again xindn"],
-  owner: "Shkar",
-};
+const abdullah = new Account(
+  "abdullah@maingroup.com",
+  111,
+  ["study web", "go to work", "sleep"],
+  "Abdullah",
+  []
+);
+
+console.log(abdullah.finishTasks);
+
+const sabr = new Account(
+  "sabr@maingroup.com",
+  222,
+  ["study design", "go to work", "watch movie"],
+  "Sabr",
+  []
+);
+
+const ayad = new Account(
+  "ayad@maingroup.com",
+  333,
+  ["study web", "Shex chaw chawanem krd", "Xo juwan krdn"],
+  "Ayad",
+  []
+);
+
+const shkar = new Account(
+  "shkar@maingroup.com",
+  444,
+  ["xindn lo awail bun", "study to be awail", "again xindn"],
+  "Shkar",
+  []
+);
 
 const accounts = [abdullah, sabr, ayad, shkar];
 
 //functions
-const updateUI = function (account) {
+const updateTasks = function (account) {
   tasksContainer.innerHTML = "";
   account.tasks.forEach((task) => {
     const html = `
@@ -65,6 +90,26 @@ const updateUI = function (account) {
     </div>
     `;
     tasksContainer.insertAdjacentHTML("afterbegin", html);
+  });
+};
+
+const updateFinishTasks = function (account) {
+  tasksContainerFinish.innerHTML = "";
+  account.finishTasks.forEach((task) => {
+    const html = `
+    <div class="task">
+       <p class="t">${task}</p>
+       <div class="btns">
+          <button class="remove">
+          <i
+             class="fa-solid fa-x"
+             style="color: #ffffff; font-size: 20px"
+             ></i>
+          </button>
+       </div>
+    </div>
+    `;
+    tasksContainerFinish.insertAdjacentHTML("afterbegin", html);
   });
 };
 // adding element to last item in arry
@@ -99,10 +144,11 @@ const greeting = function (acconut) {
     greet.textContent = `Good Morning, ${acconut.owner}`;
   } else if (hours >= 12 && hours < 18) {
     greet.textContent = `Good Afternoon, ${acconut.owner}`;
-  } else if (hours >= 18 && hours < 0) {
+  } else if (hours >= 18 && hours < 24) {
     greet.textContent = `Good Evening, ${acconut.owner}`;
   }
 
+  console.log(hours);
   const shortMonths = [
     "Jan",
     "Feb",
@@ -118,13 +164,46 @@ const greeting = function (acconut) {
     "Dec",
   ];
 
-  dateLabel.textContent = `${date.getDate()}/ ${
-    shortMonths[date.getMonth()]
-  } / ${date.getFullYear()}`;
+  dateLabel.forEach(
+    (d) =>
+      (d.textContent = `${date.getDate()}/ ${
+        shortMonths[date.getMonth()]
+      } / ${date.getFullYear()}`)
+  );
+};
+
+const switchUI = function (container) {
+  taskContainer.classList.add("hidden__container--1");
+  finishTaskContainer.classList.add("hidden__container--2");
+  taskContainer.classList.remove("container--active");
+  finishTaskContainer.classList.remove("container--active");
+
+  activeContainer =
+    activeContainer === 1 ? (activeContainer = 2) : (activeContainer = 1);
+
+  console.log(activeContainer);
+
+  document
+    .querySelector(`.container--${activeContainer}`)
+    .classList.remove(`hidden__container--${activeContainer}`);
+
+  document
+    .querySelector(`.container--${activeContainer}`)
+    .classList.add(`container--active`);
+};
+
+const updateUI = function () {
+  updateTasks(activeAccount);
+  greeting(activeAccount);
+
+  taskContainer.classList.remove("hidden__container--1");
+  taskContainer.classList.add("container--active");
+  loginAppContainer.classList.add("container--hidden");
 };
 
 // creatint active account in global scobe we might need it later
 
+let activeContainer = 1;
 let activeAccount;
 
 // event listeners
@@ -134,27 +213,31 @@ loginBtn.addEventListener("click", function (e) {
   const account = accounts.find((acc) => acc.email === emailInput.value);
   if (account.password === Number(passwordInput.value)) {
     activeAccount = account;
-    updateUI(activeAccount);
-    greeting(activeAccount);
-    appContainer.classList.remove("container--hidden");
-    loginAppContainer.classList.add("container--hidden");
+
+    updateUI();
+  } else {
+    console.log("error");
   }
 });
 
 //using event delegation
 
-tasksContainer.addEventListener("click", function (e) {
+const containerEvenListener = function (e) {
   e.preventDefault();
   if (e.target.closest(".done")) {
-    e.target
-      .closest(".task")
-      .querySelector("p")
-      .classList.add("task__finished");
+    const finish = e.target.closest(".task").querySelector("p").textContent;
+    e.target.closest(".task").remove();
+    activeAccount.finishTasks.push(finish);
+
+    updateFinishTasks(activeAccount);
   }
   if (e.target.closest(".remove")) {
     e.target.closest(".task").remove();
   }
-});
+};
+
+tasksContainer.addEventListener("click", containerEvenListener);
+finishTaskContainer.addEventListener("click", containerEvenListener);
 
 addBtn.addEventListener("click", function (e) {
   e.preventDefault();
@@ -162,3 +245,5 @@ addBtn.addEventListener("click", function (e) {
   updateLast(activeAccount);
   inputNewTask.value = "";
 });
+
+changeBtn.forEach((btn) => btn.addEventListener("click", switchUI));
